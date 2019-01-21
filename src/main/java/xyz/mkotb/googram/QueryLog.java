8c2796a6e62cb7d5291c72443e76beb919a464b3
Mazen Kotb <me@mazenk.com>
2019-01-21 00:39:57 -0800

    Switch to Date

--- src/main/java/xyz/mkotb/googram/QueryLog.java
+++ src/main/java/xyz/mkotb/googram/QueryLog.java
@@ -2,16 +2,19 @@ package xyz.mkotb.googram;
 
 import com.jtelegram.api.inline.InlineQuery;
 
+import java.time.Instant;
+import java.util.Date;
+
 public class QueryLog {
     final long userId;
     final String query;
-    final long queryDate;
+    final Date queryDate;
     final String errorMessage;
 
     public QueryLog(InlineQuery query, String errorMessage) {
         this.userId = query.getFrom().getId();
         this.query = query.getQuery();
-        this.queryDate = System.currentTimeMillis();
+        this.queryDate = Date.from(Instant.now());
         this.errorMessage = errorMessage;
     }
 }
